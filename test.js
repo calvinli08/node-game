2213c943e92ee77c65d60b7dd83982a69001d133
Calvin Li <cli.calvinli@gmail.com>
2024-02-19 20:49:33 -0500

    replace it with test in test.js

--- test.js
+++ test.js
@@ -4,41 +4,41 @@ const {chooseWinner, user, computer, tie, rock, paper, scissor} = createGame();
 
 describe('Game Tests', () => {
     //user wins
-    it('user rock beats computer scissor', () => {
+    test('user rock beats computer scissor', () => {
         expect(chooseWinner(rock, scissor)).toBe(user);
     });
 
-    it('user paper beats computer rock', () => {
+    test('user paper beats computer rock', () => {
         expect(chooseWinner(paper, rock)).toBe(user);
     });
 
-    it('user scissor beats computer paper', () => {
+    test('user scissor beats computer paper', () => {
         expect(chooseWinner(scissor, paper)).toBe(user);
     });
 
     //computer wins
-    it('computer rock beats user scissor', () => {
+    test('computer rock beats user scissor', () => {
         expect(chooseWinner(scissor, rock)).toBe(computer);
     });
 
-    it('computer paper beats user rock', () => {
+    test('computer paper beats user rock', () => {
         expect(chooseWinner(rock, paper)).toBe(computer);
     });
 
-    it('computer scissor beats user paper', () => {
+    test('computer scissor beats user paper', () => {
         expect(chooseWinner(paper, scissor)).toBe(computer);
     });
 
     //ties
-    it('rock tie', () => {
+    test('rock tie', () => {
         expect(chooseWinner(rock, rock)).toBe(tie);
     });
 
-    it('paper tie', () => {
+    test('paper tie', () => {
         expect(chooseWinner(paper, paper)).toBe(tie);
     });
 
-    it('scissor tie', () => {
+    test('scissor tie', () => {
         expect(chooseWinner(scissor, scissor)).toBe(tie);
     });
 });
